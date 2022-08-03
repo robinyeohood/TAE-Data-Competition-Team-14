2072de02b2daa748255c6e23ddbf55d9c92a08e5
robinyeohood <robinyeo.hood@gmail.com>
2022-08-03 22:22:39 +0800

    Use prepare_data() to help prepare your data!

--- prepare_data.R
+++ prepare_data.R
@@ -5,7 +5,7 @@
 # minimum percentage appearance (a number between 0 and 0.001 for removing sparse terms, else default value of 0 is set)
 
 # OUTPUT
-# The processed data frame that can be split.
+# The processed data frame that can be split (you'll have to split the data after running this function).
 
 # Why I used [[]] instead of $:
 # https://stackoverflow.com/questions/2641653/pass-a-data-frame-column-name-to-a-function
@@ -17,10 +17,6 @@ prepare_data <- function(csv_file, text_column_name, dependent_variable, min_per
     install.packages("tm")
     library(tm)
   }
-  # if(!require(dplyr)){ # install or load the package
-  #   install.packages("dplyr")
-  #   library(dplyr)
-  # }
   corpus <- Corpus(VectorSource(data[[text_column_name]])) # Build a new corpus object called 'corpus'
   corpus <- tm_map(corpus, content_transformer(tolower)) # Convert text to lower case
   corpus <- tm_map(corpus, removePunctuation) # Remove all punctuation
@@ -39,25 +35,25 @@ prepare_data <- function(csv_file, text_column_name, dependent_variable, min_per
   
 }
 
-testing <- prepare_data('train.csv', 'tweet', 'sentiment', 0.001)
+# testing <- prepare_data('train.csv', 'tweet', 'sentiment', 0.001)
 
 ########## Debugging ##########
 
-train <- read.csv("train.csv", stringsAsFactors = FALSE) # Load the data
-if(!require(tm)){ # install or load the package
-  install.packages("tm")
-  library(tm)
-}
-corpus2 <- Corpus(VectorSource(train$tweet)) # Build a new corpus2 object called 'corpus2'
-corpus2 <- tm_map(corpus2, content_transformer(tolower)) # Convert text to lower case
-corpus2 <- tm_map(corpus2, removePunctuation) # Remove all punctuation
-corpus2 <- tm_map(corpus2, removeWords, stopwords("english")) # Remove all English stop words from the corpus2. Can modify to remove additional words.
-corpus2 <- tm_map(corpus2, stemDocument) # Stem the words in the corpus2
-dtm2 <- DocumentTermMatrix(corpus2) # Build the document term matrix (dtm) from the corpus2
-sparse_dtm2 <- removeSparseTerms(dtm2, 0.999) # Remove highly infrequent terms. Setting a sparsity of 0.95 cause the number of terms to fall from 19248 to 8.
-processed_data <- as.data.frame(as.matrix(sparse_dtm2)) # Build a data frame from the dtm
-colnames(processed_data) <- make.names(colnames(processed_data)) # Make the variable names of the dtm data frame valid
-processed_data$sentiment <- train$sentiment # Add the dependent variable back to the dtm data frame
-processed_data$sentiment <- as.factor(processed_data$sentiment) # Convert the dependent variable to a factor
-
-View(processed_data) # return the processed data frame
+# train <- read.csv("train.csv", stringsAsFactors = FALSE) # Load the data
+# if(!require(tm)){ # install or load the package
+#   install.packages("tm")
+#   library(tm)
+# }
+# corpus2 <- Corpus(VectorSource(train$tweet)) # Build a new corpus2 object called 'corpus2'
+# corpus2 <- tm_map(corpus2, content_transformer(tolower)) # Convert text to lower case
+# corpus2 <- tm_map(corpus2, removePunctuation) # Remove all punctuation
+# corpus2 <- tm_map(corpus2, removeWords, stopwords("english")) # Remove all English stop words from the corpus2. Can modify to remove additional words.
+# corpus2 <- tm_map(corpus2, stemDocument) # Stem the words in the corpus2
+# dtm2 <- DocumentTermMatrix(corpus2) # Build the document term matrix (dtm) from the corpus2
+# sparse_dtm2 <- removeSparseTerms(dtm2, 0.999) # Remove highly infrequent terms. Setting a sparsity of 0.95 cause the number of terms to fall from 19248 to 8.
+# processed_data <- as.data.frame(as.matrix(sparse_dtm2)) # Build a data frame from the dtm
+# colnames(processed_data) <- make.names(colnames(processed_data)) # Make the variable names of the dtm data frame valid
+# processed_data$sentiment <- train$sentiment # Add the dependent variable back to the dtm data frame
+# processed_data$sentiment <- as.factor(processed_data$sentiment) # Convert the dependent variable to a factor
+# 
+# View(processed_data) # return the processed data frame
